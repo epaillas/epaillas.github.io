4459a03f0071a110862a4a8aa4c6baee869098c0
epaillas <epaillas@arizona.edu>
2025-02-22 20:23:46 +0000

    Deploying to gh-pages from @ epaillas/epaillas.github.io@32e92d709a89f378b9cfb21add7cebbf8b5e941e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
